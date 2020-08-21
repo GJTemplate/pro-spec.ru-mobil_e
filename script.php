89cc54b69b8863b7629736e08547dca9392f6989
oleg <sad.net79@gmail.com>
2020-08-21 18:29:31 +0300

    version > 1.1.5

--- script.php
+++ script.php
@@ -58,23 +58,6 @@ class mobil_eInstallerScript
     public function __construct()
     {
         $this->app = Factory::getApplication();
-
-        /**
-         * Загрузка языкового файла
-         */
-        $language =  Factory::getLanguage();
-//        $language->load('tpl_mobil_e' , __DIR__.'/language' , $language->getTag(), true);
-//        $language->load('tpl_mobil_e' , dirname(__FILE__).'' , $language->getTag(), true);
-
-
-
-         echo'<pre>';print_r(  dirname(__FILE__) );echo'</pre>'.__FILE__.' '.__LINE__;
-         echo'<pre>';print_r(  __DIR__ );echo'</pre>'.__FILE__.' '.__LINE__;
-         echo'<pre>';print_r(  $language );echo'</pre>'.__FILE__.' '.__LINE__;
-         echo'<pre>';print_r( Text::_('TEMPLATE_DETAILS_DEBUG') );echo'</pre>'.__FILE__.' '.__LINE__;
-         die(__FILE__ .' '. __LINE__ );
-
-
     }
 
     /**
